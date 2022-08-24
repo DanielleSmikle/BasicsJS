8b0e15a24335846c982d48e7c33bed75e53a196a
Username <daniellesmikle@Danielles-Air.home>
2022-08-23 20:52:19 -0400

    practing functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,21 +3,17 @@ let currentResult = defaultResult;
 
 
 
-function add(num1, num2){
+function add(num1, num2) {
     const result = num1 + num2;
-    alert('The result is' + result);
-    return result;
-
+    return result; 
 }
-const additionResult = add(1,2);
-add(5,5);
-currentResult= additionResult;
 
-let calculationDescription = `(${defaultResult} +10) 
+currentResult = add(1,2);
 
 
 
-* 3/2 -1`;
 
+let calculationDescription = `(${defaultResult} +10) *3/2-1`;
 
 outputResult(currentResult, calculationDescription);
+
